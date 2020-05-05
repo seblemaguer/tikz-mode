0ac941928f8322b6b361b67b362ec6d9e41b8370
Sebastien Le Maguer <lemagues@tcd.ie>
2020-05-05 23:32:06 +0100

    [tikz-mode] add: tikz-mode.el

---
+++ tikz-mode.el
@@ -0,0 +1,47 @@
+;;; tikz-mode.el ---  Mode to edit tikz file -*- lexical-binding: t; coding: utf-8 -*-
+
+;; Copyright (C)  5 May 2020
+;;
+
+;; Author: Sébastien Le Maguer <sebastien.lemaguer@adaptcentre.ie>
+
+;; Package-Requires: ((emacs "25.2"))
+;; Keywords:
+;; Homepage:
+
+;; tikz-mode is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; tikz-mode is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with tikz-mode.  If not, see http://www.gnu.org/licenses.
+
+;;; Commentary:
+
+
+;;; Code:
+(defun auctex-compilePGF-setup ()
+  "Add LatexMk command to TeX-command-list."
+  (setq-default TeX-command-list  (cons
+                                   '("CompilePGF" "compilePGF %t"
+                                     TeX-run-command t (tikz-mode)
+                                     :help "Run compilePGF to compile TIKZ/PGF like ktikz")
+                                   TeX-command-list)))
+
+
+(define-derived-mode tikz-mode latex-mode "Tikz/PGF"
+  "PGF/Tikz dedicated mode"
+  (setq-default TeX-file-extensions (append '("pgf" "tikz") TeX-file-extensions))
+  (auctex-compilePGF-setup))
+
+(add-to-list 'auto-mode-alist '("\\.\\(pgf\\|tikz\\)" . tikz-mode))
+
+(provide 'tikz-mode)
+
+;;; tikz-mode.el ends here
